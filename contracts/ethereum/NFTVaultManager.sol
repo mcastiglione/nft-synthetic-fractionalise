33906e65c14c0aa16640cefbd6065ad6d0c9d565
Eric Nordelo <eric.nordelo39@gmail.com>
2021-09-15 15:33:20 -0400

    feat: add vault logic

--- contracts/ethereum/NFTVaultManager.sol
+++ contracts/ethereum/NFTVaultManager.sol
@@ -1,6 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract NFTVaultManager {
-    constructor() {}    
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "hardhat/console.sol";
+
+contract NFTVaultManager is IERC721Receiver {
+    /**
+     * @notice the whitelist for the NFT collection addresses accpeted
+     */
+    mapping(address => bool) public approvedCollections;
+
+    /**
+     * @dev map to check if a holder has a token registered over an approved collection
+     *
+     *  COLLECTION_CONTRACT_ADDRESS => NFT_ID => HOLDER_ACCOUNT_ADDRESS OR ZERO_ADDRESS
+     */
+    mapping(address => mapping(uint256 => address)) private _holdings;
+
+    /**
+     * @dev See {IERC721Receiver-onERC721Received}.
+     */
+    function onERC721Received(
+        address operator_,
+        address,
+        uint256 tokenId_,
+        bytes memory
+    ) public virtual override returns (bytes4) {
+        require(approvedCollections[msg.sender], "Not approved collection");
+
+        // this should be an invariant (can't receive a token that the contract is already holding)
+        assert(_holdings[msg.sender][tokenId_] == address(0));
+
+        // the sender must be the collection contract
+        _holdings[msg.sender][tokenId_] == operator_;
+
+        return this.onERC721Received.selector;
+    }
+
+    /**
+     * @notice check if the vault holds a token
+     */
+    function isTokenInVault(address collection_, uint256 tokenId_) public view returns (bool) {
+        require(approvedCollections[collection_], "Not approved collection");
+
+        address previousOwner = _holdings[collection_][tokenId_];
+        return previousOwner != address(0);
+    }
 }
