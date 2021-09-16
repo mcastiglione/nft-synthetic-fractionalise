93ad68821ee5d33d51c4fa4dc3e49fce83a5e32b
Eric Nordelo <eric.nordelo39@gmail.com>
2021-09-16 12:07:20 -0400

    feat: add erc20permit to jix token

--- contracts/polygon/governance/JIX.sol
+++ contracts/polygon/governance/JIX.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract JIX is ERC20, ERC20Votes {
+contract JIX is ERC20, ERC20Permit, ERC20Votes {
     // solhint-disable-next-line
     constructor() ERC20("Privi JIX", "JIX") ERC20Permit("Privi JIX") {}
 
