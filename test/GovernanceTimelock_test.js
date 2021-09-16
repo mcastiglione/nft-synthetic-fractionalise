bbbf187c354ec5f372f553e37119be6fddd7079e
Eric Nordelo <eric.nordelo39@gmail.com>
2021-09-16 18:02:16 -0400

    feat: add more tests to timelock and governance

--- test/GovernanceTimelock_test.js
+++ test/GovernanceTimelock_test.js
@@ -51,7 +51,7 @@ describe('GovernorTimelockControl', function () {
     });
 
     afterEach(async function () {
-      const timelockid = await this.timelock.hashOperationBatch(
+      await this.timelock.hashOperationBatch(
         ...this.settings.proposal.slice(0, 3),
         web3.eth.abi.encodeParameter('bytes32', '0x0'),
         this.descriptionHash
@@ -68,220 +68,233 @@ describe('GovernorTimelockControl', function () {
     runGovernorWorkflow();
   });
 
-  // describe('executed by other proposer', function () {
-  //   beforeEach(async function () {
-  //     this.settings = {
-  //       proposal: [
-  //         [this.receiver.address],
-  //         [web3.utils.toWei('0')],
-  //         [this.receiver.contract.methods.mockFunction().encodeABI()],
-  //         '<proposal description>',
-  //       ],
-  //       voters: [{ voter: voter, support: Enums.VoteType.For }],
-  //       steps: {
-  //         queue: { delay: 3600 },
-  //         execute: { enable: false },
-  //       },
-  //     };
-  //   });
-  //   afterEach(async function () {
-  //     await this.timelock.executeBatch(...this.settings.proposal.slice(0, 3), '0x0', this.descriptionHash);
+  describe('executed by other proposer', function () {
+    beforeEach(async function () {
+      this.settings = {
+        proposal: [
+          [this.receiver.address],
+          [web3.utils.toWei('0')],
+          [this.receiver.interface.encodeFunctionData('mockFunction', [])],
+          '<proposal description>',
+        ],
+        voters: [{ voter: voter, support: Enums.VoteType.For }],
+        steps: {
+          queue: { delay: 3600 },
+          execute: { enable: false },
+        },
+      };
+    });
 
-  //     expect(await this.mock.state(this.id)).to.be.bignumber.equal(Enums.ProposalState.Executed);
+    afterEach(async function () {
+      await this.timelock.executeBatch(
+        ...this.settings.proposal.slice(0, 3),
+        web3.eth.abi.encodeParameter('bytes32', '0x0'),
+        this.descriptionHash
+      );
 
-  //     await expectRevert(
-  //       this.mock.execute(...this.settings.proposal.slice(0, -1), this.descriptionHash),
-  //       'Governor: proposal not successful'
-  //     );
-  //   });
-  //   runGovernorWorkflow();
-  // });
+      expect(String(await this.mock.state(this.id))).to.be.bignumber.equal(Enums.ProposalState.Executed);
 
-  // describe('not queued', function () {
-  //   beforeEach(async function () {
-  //     this.settings = {
-  //       proposal: [
-  //         [this.receiver.address],
-  //         [web3.utils.toWei('0')],
-  //         [this.receiver.contract.methods.mockFunction().encodeABI()],
-  //         '<proposal description>',
-  //       ],
-  //       voters: [{ voter: voter, support: Enums.VoteType.For }],
-  //       steps: {
-  //         queue: { enable: false },
-  //         execute: { error: 'TimelockController: operation is not ready' },
-  //       },
-  //     };
-  //   });
-  //   afterEach(async function () {
-  //     expect(await this.mock.state(this.id)).to.be.bignumber.equal(Enums.ProposalState.Succeeded);
-  //   });
-  //   runGovernorWorkflow();
-  // });
+      await expect(this.mock.execute(...this.settings.proposal.slice(0, -1), this.descriptionHash)).to.be.revertedWith(
+        'Governor: proposal not successful'
+      );
+    });
 
-  // describe('to early', function () {
-  //   beforeEach(async function () {
-  //     this.settings = {
-  //       proposal: [
-  //         [this.receiver.address],
-  //         [web3.utils.toWei('0')],
-  //         [this.receiver.contract.methods.mockFunction().encodeABI()],
-  //         '<proposal description>',
-  //       ],
-  //       voters: [{ voter: voter, support: Enums.VoteType.For }],
-  //       steps: {
-  //         execute: { error: 'TimelockController: operation is not ready' },
-  //       },
-  //     };
-  //   });
-  //   afterEach(async function () {
-  //     expect(await this.mock.state(this.id)).to.be.bignumber.equal(Enums.ProposalState.Queued);
-  //   });
-  //   runGovernorWorkflow();
-  // });
+    runGovernorWorkflow();
+  });
 
-  // describe('re-queue / re-execute', function () {
-  //   beforeEach(async function () {
-  //     this.settings = {
-  //       proposal: [
-  //         [this.receiver.address],
-  //         [web3.utils.toWei('0')],
-  //         [this.receiver.contract.methods.mockFunction().encodeABI()],
-  //         '<proposal description>',
-  //       ],
-  //       voters: [{ voter: voter, support: Enums.VoteType.For }],
-  //       steps: {
-  //         queue: { delay: 3600 },
-  //       },
-  //     };
-  //   });
-  //   afterEach(async function () {
-  //     expect(await this.mock.state(this.id)).to.be.bignumber.equal(Enums.ProposalState.Executed);
+  describe('not queued', function () {
+    beforeEach(async function () {
+      this.settings = {
+        proposal: [
+          [this.receiver.address],
+          [web3.utils.toWei('0')],
+          [this.receiver.interface.encodeFunctionData('mockFunction', [])],
+          '<proposal description>',
+        ],
+        voters: [{ voter: voter, support: Enums.VoteType.For }],
+        steps: {
+          queue: { enable: false },
+          execute: { error: 'TimelockController: operation is not ready' },
+        },
+      };
+    });
 
-  //     await expectRevert(
-  //       this.mock.queue(...this.settings.proposal.slice(0, -1), this.descriptionHash),
-  //       'Governor: proposal not successful'
-  //     );
-  //     await expectRevert(
-  //       this.mock.execute(...this.settings.proposal.slice(0, -1), this.descriptionHash),
-  //       'Governor: proposal not successful'
-  //     );
-  //   });
-  //   runGovernorWorkflow();
-  // });
+    afterEach(async function () {
+      expect(String(await this.mock.state(this.id))).to.be.bignumber.equal(Enums.ProposalState.Succeeded);
+    });
 
-  // describe('cancel before queue prevents scheduling', function () {
-  //   beforeEach(async function () {
-  //     this.settings = {
-  //       proposal: [
-  //         [this.receiver.address],
-  //         [web3.utils.toWei('0')],
-  //         [this.receiver.contract.methods.mockFunction().encodeABI()],
-  //         '<proposal description>',
-  //       ],
-  //       voters: [{ voter: voter, support: Enums.VoteType.For }],
-  //       steps: {
-  //         queue: { enable: false },
-  //         execute: { enable: false },
-  //       },
-  //     };
-  //   });
-  //   afterEach(async function () {
-  //     expect(await this.mock.state(this.id)).to.be.bignumber.equal(Enums.ProposalState.Succeeded);
+    runGovernorWorkflow();
+  });
 
-  //     expectEvent(
-  //       await this.mock.cancel(...this.settings.proposal.slice(0, -1), this.descriptionHash),
-  //       'ProposalCanceled',
-  //       { proposalId: this.id }
-  //     );
+  describe('to early', function () {
+    beforeEach(async function () {
+      this.settings = {
+        proposal: [
+          [this.receiver.address],
+          [web3.utils.toWei('0')],
+          [this.receiver.interface.encodeFunctionData('mockFunction', [])],
+          '<proposal description>',
+        ],
+        voters: [{ voter: voter, support: Enums.VoteType.For }],
+        steps: {
+          execute: { error: 'TimelockController: operation is not ready' },
+        },
+      };
+    });
 
-  //     expect(await this.mock.state(this.id)).to.be.bignumber.equal(Enums.ProposalState.Canceled);
+    afterEach(async function () {
+      expect(String(await this.mock.state(this.id))).to.be.bignumber.equal(Enums.ProposalState.Queued);
+    });
 
-  //     await expectRevert(
-  //       this.mock.queue(...this.settings.proposal.slice(0, -1), this.descriptionHash),
-  //       'Governor: proposal not successful'
-  //     );
-  //   });
-  //   runGovernorWorkflow();
-  // });
+    runGovernorWorkflow();
+  });
 
-  // describe('cancel after queue prevents execution', function () {
-  //   beforeEach(async function () {
-  //     this.settings = {
-  //       proposal: [
-  //         [this.receiver.address],
-  //         [web3.utils.toWei('0')],
-  //         [this.receiver.contract.methods.mockFunction().encodeABI()],
-  //         '<proposal description>',
-  //       ],
-  //       voters: [{ voter: voter, support: Enums.VoteType.For }],
-  //       steps: {
-  //         queue: { delay: 3600 },
-  //         execute: { enable: false },
-  //       },
-  //     };
-  //   });
-  //   afterEach(async function () {
-  //     const timelockid = await this.timelock.hashOperationBatch(
-  //       ...this.settings.proposal.slice(0, 3),
-  //       '0x0',
-  //       this.descriptionHash
-  //     );
+  describe('re-queue / re-execute', function () {
+    beforeEach(async function () {
+      this.settings = {
+        proposal: [
+          [this.receiver.address],
+          [web3.utils.toWei('0')],
+          [this.receiver.interface.encodeFunctionData('mockFunction', [])],
+          '<proposal description>',
+        ],
+        voters: [{ voter: voter, support: Enums.VoteType.For }],
+        steps: {
+          queue: { delay: 3600 },
+        },
+      };
+    });
 
-  //     expect(await this.mock.state(this.id)).to.be.bignumber.equal(Enums.ProposalState.Queued);
+    afterEach(async function () {
+      expect(String(await this.mock.state(this.id))).to.be.bignumber.equal(Enums.ProposalState.Executed);
 
-  //     const receipt = await this.mock.cancel(...this.settings.proposal.slice(0, -1), this.descriptionHash);
-  //     expectEvent(receipt, 'ProposalCanceled', { proposalId: this.id });
-  //     await expectEvent.inTransaction(receipt.receipt.transactionHash, this.timelock, 'Cancelled', { id: timelockid });
+      await expect(this.mock.queue(...this.settings.proposal.slice(0, -1), this.descriptionHash)).to.be.revertedWith(
+        'Governor: proposal not successful'
+      );
 
-  //     expect(await this.mock.state(this.id)).to.be.bignumber.equal(Enums.ProposalState.Canceled);
+      await expect(this.mock.execute(...this.settings.proposal.slice(0, -1), this.descriptionHash)).to.be.revertedWith(
+        'Governor: proposal not successful'
+      );
+    });
 
-  //     await expectRevert(
-  //       this.mock.execute(...this.settings.proposal.slice(0, -1), this.descriptionHash),
-  //       'Governor: proposal not successful'
-  //     );
-  //   });
-  //   runGovernorWorkflow();
-  // });
+    runGovernorWorkflow();
+  });
 
-  // describe('updateTimelock', function () {
-  //   beforeEach(async function () {
-  //     this.newTimelock = await deployments.deploy('newTimelockController', {
-  //       contract: 'TimelockController',
-  //       from: deployer,
-  //       args: [3600, [], []],
-  //     });
-  //   });
+  describe('cancel before queue prevents scheduling', function () {
+    beforeEach(async function () {
+      this.settings = {
+        proposal: [
+          [this.receiver.address],
+          [web3.utils.toWei('0')],
+          [this.receiver.interface.encodeFunctionData('mockFunction', [])],
+          '<proposal description>',
+        ],
+        voters: [{ voter: voter, support: Enums.VoteType.For }],
+        steps: {
+          queue: { enable: false },
+          execute: { enable: false },
+        },
+      };
+    });
 
-  //   it('protected', async function () {
-  //     await expectRevert(this.mock.updateTimelock(this.newTimelock.address), 'Governor: onlyGovernance');
-  //   });
+    afterEach(async function () {
+      expect(String(await this.mock.state(this.id))).to.be.bignumber.equal(Enums.ProposalState.Succeeded);
 
-  //   describe('using workflow', function () {
-  //     beforeEach(async function () {
-  //       this.settings = {
-  //         proposal: [
-  //           [this.mock.address],
-  //           [web3.utils.toWei('0')],
-  //           [this.mock.contract.methods.updateTimelock(this.newTimelock.address).encodeABI()],
-  //           '<proposal description>',
-  //         ],
-  //         voters: [{ voter: voter, support: Enums.VoteType.For }],
-  //         steps: {
-  //           queue: { delay: 3600 },
-  //         },
-  //       };
-  //     });
-  //     afterEach(async function () {
-  //       expectEvent(this.receipts.propose, 'ProposalCreated', { proposalId: this.id });
-  //       expectEvent(this.receipts.execute, 'ProposalExecuted', { proposalId: this.id });
-  //       expectEvent(this.receipts.execute, 'TimelockChange', {
-  //         oldTimelock: this.timelock.address,
-  //         newTimelock: this.newTimelock.address,
-  //       });
-  //       expect(await this.mock.timelock()).to.be.bignumber.equal(this.newTimelock.address);
-  //     });
-  //     runGovernorWorkflow();
-  //   });
-  // });
+      expect(await this.mock.cancel(...this.settings.proposal.slice(0, -1), this.descriptionHash)).to.emit(
+        this.mock,
+        'ProposalCanceled'
+      );
+
+      expect(String(await this.mock.state(this.id))).to.be.bignumber.equal(Enums.ProposalState.Canceled);
+
+      await expect(this.mock.queue(...this.settings.proposal.slice(0, -1), this.descriptionHash)).to.be.revertedWith(
+        'Governor: proposal not successful'
+      );
+    });
+
+    runGovernorWorkflow();
+  });
+
+  describe('cancel after queue prevents execution', function () {
+    beforeEach(async function () {
+      this.settings = {
+        proposal: [
+          [this.receiver.address],
+          [web3.utils.toWei('0')],
+          [this.receiver.interface.encodeFunctionData('mockFunction', [])],
+          '<proposal description>',
+        ],
+        voters: [{ voter: voter, support: Enums.VoteType.For }],
+        steps: {
+          queue: { delay: 3600 },
+          execute: { enable: false },
+        },
+      };
+    });
+
+    afterEach(async function () {
+      await this.timelock.hashOperationBatch(
+        ...this.settings.proposal.slice(0, 3),
+        web3.eth.abi.encodeParameter('bytes32', '0x0'),
+        this.descriptionHash
+      );
+
+      expect(String(await this.mock.state(this.id))).to.be.bignumber.equal(Enums.ProposalState.Queued);
+
+      const receipt = this.mock.cancel(...this.settings.proposal.slice(0, -1), this.descriptionHash);
+      await expect(receipt).to.emit(this.mock, 'ProposalCanceled');
+      await expect(receipt).to.emit(this.timelock, 'Cancelled');
+
+      expect(String(await this.mock.state(this.id))).to.be.bignumber.equal(Enums.ProposalState.Canceled);
+
+      await expect(this.mock.execute(...this.settings.proposal.slice(0, -1), this.descriptionHash)).to.be.revertedWith(
+        'Governor: proposal not successful'
+      );
+    });
+
+    runGovernorWorkflow();
+  });
+
+  describe('updateTimelock', function () {
+    beforeEach(async function () {
+      const { deployer } = await getNamedAccounts();
+
+      this.newTimelock = await deployments.deploy('newTimelockController', {
+        contract: 'TimelockController',
+        from: deployer,
+        args: [3600, [], []],
+      });
+    });
+
+    it('protected', async function () {
+      await expect(this.mock.updateTimelock(this.newTimelock.address)).to.be.revertedWith('Governor: onlyGovernance');
+    });
+
+    describe('using workflow', function () {
+      beforeEach(async function () {
+        this.settings = {
+          proposal: [
+            [this.mock.address],
+            [web3.utils.toWei('0')],
+            [this.mock.interface.encodeFunctionData('updateTimelock', [this.newTimelock.address])],
+            '<proposal description>',
+          ],
+          voters: [{ voter: voter, support: Enums.VoteType.For }],
+          steps: {
+            queue: { delay: 3600 },
+          },
+        };
+      });
+
+      afterEach(async function () {
+        await expect(this.receipts.propose).to.emit(this.mock, 'ProposalCreated');
+        await expect(this.receipts.execute).to.emit(this.mock, 'ProposalExecuted');
+        await expect(this.receipts.execute).to.emit(this.mock, 'TimelockChange');
+
+        expect(String(await this.mock.timelock())).to.be.bignumber.equal(this.newTimelock.address);
+      });
+
+      runGovernorWorkflow();
+    });
+  });
 });
