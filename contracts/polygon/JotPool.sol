72a5d6e01798fbddc063ad186ba50d4994ea0e42
Piotr Buda <pibuda@gmail.com>
2021-09-16 21:06:46 +0200

    Updates to JotPool removeLiquidity

--- contracts/polygon/JotPool.sol
+++ contracts/polygon/JotPool.sol
@@ -13,6 +13,9 @@ contract JotPool {
 
     address public immutable jot;
 
+    event LiquidityAdded(address provider, uint256 amount, uint256 mintedLiquidity);
+    event LiquidityRemoved(address provider, uint256 amount, uint256 liquidityBurnt);
+
     constructor(address _jot) {
         jot = _jot;
     }
@@ -24,15 +27,27 @@ contract JotPool {
             : (totalLiquidity * amount) / IERC20(jot).balanceOf(address(this));
         liquidity[msg.sender] += mintedLiquidity;
         totalLiquidity += mintedLiquidity;
+        emit LiquidityAdded(msg.sender, amount, mintedLiquidity);
         IERC20(jot).safeTransferFrom(msg.sender, address(this), amount);
     }
 
     function removeLiquidity(uint256 amount) external {
-        require(liquidity[msg.sender] >= amount, "Insufficient balance");
-        uint256 burntLiquidity = (IERC20(jot).balanceOf(address(this)) * liquidity[msg.sender]) /
-            totalLiquidity;
-        liquidity[msg.sender] -= amount;
-        totalLiquidity -= amount;
-        IERC20(jot).safeTransfer(msg.sender, burntLiquidity);
+        require(liquidity[msg.sender] >= amount, "Remove amount exceeds balance");
+        uint256 liquidityBurnt = (IERC20(jot).balanceOf(address(this)) * amount) / totalLiquidity;
+        if (totalLiquidity - amount == 0) {
+            liquidity[msg.sender] = 100;
+            totalLiquidity = 100;
+        } else {
+            liquidity[msg.sender] -= amount;
+            totalLiquidity -= amount;
+        }
+
+        emit LiquidityRemoved(msg.sender, amount, liquidityBurnt);
+
+        IERC20(jot).safeTransfer(msg.sender, liquidityBurnt);
+    }
+
+    function balance() external view returns (uint256) {
+        return liquidity[msg.sender];
     }
 }
--- test/JotPool_test.js
+++ test/JotPool_test.js
@@ -1,6 +1,14 @@
-const { assert } = require('chai');
+const { assert, expect } = require('chai');
+const { expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
 
 describe('JotPool', async function () {
+  const addLiquidity = async (amount) => {
+    await jot.approve(pool.address, amount);
+    return await pool.addLiquidity(amount);
+  };
+
+  const jotAmount = (amount) => ethers.utils.parseEther(amount);
+
   beforeEach(async () => {
     // Using fixture from hardhat-deploy
     await deployments.fixture(['jot', 'jot_pool']);
@@ -14,22 +22,68 @@ describe('JotPool', async function () {
 
   describe('addLiquidity', () => {
     it('should mint 100 shares on initial', async () => {
-      const amount = ethers.utils.parseEther('200');
-      await jot.approve(pool.address, amount);
-      await pool.addLiquidity(amount);
+      const amount = jotAmount('200');
+      await addLiquidity(amount);
 
       assert.equal(await pool.totalLiquidity(), 100);
+      assert.equal(await pool.balance(), 100);
     });
-    it('should transfer proper amount of jot to pool', async () => {
-      const amount = ethers.utils.parseEther('200');
-      await jot.approve(pool.address, amount);
-      await pool.addLiquidity(amount);
 
-      assert.equal(await jot.balanceOf(pool.address), amount);
+    it('should transfer proper amount of jot to pool', async () => {
+      const amount = jotAmount('200');
+      await addLiquidity(amount);
+
+      assert.deepEqual(await jot.balanceOf(pool.address), amount);
+    });
+
+    it('should emit LiquidityAdded event', async () => {
+      const { deployer } = await getNamedAccounts();
+      const amount = jotAmount('200');
+      await jot.approve(pool.address, amount);
+
+      await expect(pool.addLiquidity(amount)).to.emit(pool, 'LiquidityAdded').withArgs(deployer, amount, 100);
     });
   });
 
   describe('removeLiquidity', () => {
-    it('should remove specified liquidity', async () => {});
+    it('should not allow to remove more than allowed balance', async () => {
+      await expectRevert(pool.removeLiquidity(1), 'Remove amount exceeds balance');
+    });
+
+    it('should remove specified liquidity', async () => {
+      const addAmount = jotAmount('200');
+      const removeAmount = 50;
+      const expectedTotalLiquidity = 50;
+      const expectedBalance = 50;
+      await addLiquidity(addAmount);
+
+      await pool.removeLiquidity(removeAmount);
+
+      assert.equal(await pool.totalLiquidity(), expectedTotalLiquidity);
+      assert.equal(await pool.balance(), expectedBalance);
+    });
+
+    it('should emit LiquidityRemoved event', async () => {
+      const { deployer } = await getNamedAccounts();
+      const addAmount = jotAmount('200');
+      const removeAmount = 50;
+      const expectedLiquidityBurnt = ethers.utils.parseEther('100');
+      await addLiquidity(addAmount);
+
+      await expect(pool.removeLiquidity(removeAmount))
+        .to.emit(pool, 'LiquidityRemoved')
+        .withArgs(deployer, removeAmount, expectedLiquidityBurnt);
+    });
+
+    it('should transfer jot on remove', async () => {
+      const addAmount = jotAmount('200');
+      const removeAmount = 50;
+      const expectedJotBalance = jotAmount('100');
+      await addLiquidity(addAmount);
+
+      await pool.removeLiquidity(removeAmount);
+
+      assert.deepEqual(await jot.balanceOf(pool.address), expectedJotBalance);
+    });
   });
 });
