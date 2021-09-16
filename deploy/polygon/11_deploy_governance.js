9ba11f55193be710c15092ecd3c8975c16038ffd
Eric Nordelo <eric.nordelo39@gmail.com>
2021-09-16 12:05:21 -0400

    feat: add governance logic with migrations

---
+++ deploy/polygon/11_deploy_governance.js
@@ -0,0 +1,25 @@
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  // keccak256 combined with bytes conversion (identity function)
+  const PROPOSER_ROLE = ethers.utils.id('PROPOSER_ROLE');
+  const ADMIN_ROLE = ethers.utils.id('TIMELOCK_ADMIN_ROLE');
+
+  // get the previously deployed contracts
+  let token = await ethers.getContract('JIX');
+  let timelock = await ethers.getContract('TimelockController');
+
+  let governance = await deploy('Governance', {
+    from: deployer,
+    log: true,
+    args: [token.address, timelock.address],
+  });
+
+  // give the proposer role to governance and renounce admin role
+  timelock.grantRole(PROPOSER_ROLE, governance.address);
+  timelock.renounceRole(ADMIN_ROLE, deployer);
+};
+
+module.exports.tags = ['governance'];
+module.exports.dependencies = ['jix_token', 'timelock_controller'];
--- test/Governance_test.js
+++ test/Governance_test.js
@@ -1,14 +1,14 @@
 const { assert } = require('chai');
 
 describe('Governance', async function () {
-  // beforeEach(async () => {
-  //   // Using fixture from hardhat-deploy
-  //   await deployments.fixture(['governance']);
+  beforeEach(async () => {
+    // Using fixture from hardhat-deploy
+    await deployments.fixture(['governance']);
 
-  //   governance = await ethers.getContract('Governance');
-  // });
+    governance = await ethers.getContract('Governance');
+  });
 
   it('should be deployed', async () => {
-    // assert.ok(governance.address);
+    assert.ok(governance.address);
   });
 });
