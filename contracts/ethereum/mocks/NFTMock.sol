b42e204af264680d5e55378c78e6639397ba8470
Eric Nordelo <eric.nordelo39@gmail.com>
2021-09-24 13:26:53 -0400

    feat: add nft mock and more tests

---
+++ contracts/ethereum/mocks/NFTMock.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFTMock is ERC721 {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("MyToken", "MTK") {}
+
+    function safeMint(address to) public {
+        _tokenIdCounter.increment();
+        _safeMint(to, _tokenIdCounter.current());
+    }
+}
--- deploy/ethereum/01_deploy_vault.js
+++ deploy/ethereum/01_deploy_vault.js
@@ -2,6 +2,15 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
+  // deploy NFT mock in local networks
+  if (network.tags.local) {
+    await deploy('NFTMock', {
+      from: deployer,
+      log: true,
+      args: [],
+    });
+  }
+
   await deploy('NFTVaultManager', {
     from: deployer,
     log: true,
