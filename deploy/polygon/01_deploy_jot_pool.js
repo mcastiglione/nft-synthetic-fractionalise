adece11fe54ea46c4f7114ab9a2c140a4f0f612f
Piotr Buda <pibuda@gmail.com>
2021-09-24 19:33:41 +0200

    Fixes initial liquidity configuration

--- deploy/polygon/01_deploy_jot_pool.js
+++ deploy/polygon/01_deploy_jot_pool.js
@@ -2,11 +2,14 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
+  let protocol = await ethers.getContract('ProtocolParameters');
+
   await deploy('JotPool', {
     from: deployer,
     log: true,
-    args: [],
+    args: [protocol.address],
   });
 };
 
 module.exports.tags = ['jot_pool_implementation'];
+module.exports.dependencies = ['protocol_parameters'];
