656db88566354a979740be422ba5c18e8423f1ec
mcastiglione <marcocastiglione5@gmail.com>
2021-09-24 23:48:23 -0300

    added some tests to synthetic NFT

--- deploy/polygon/04_deploy_synthetic_nft.js
+++ deploy/polygon/04_deploy_synthetic_nft.js
@@ -2,11 +2,23 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  await deploy('SyntheticNFT', {
-    from: deployer,
-    log: true,
-    args: [],
-  });
+  // in local network for testing deploy the mock  
+  if (network.tags.local) {
+    await deploy('SyntheticNFT', {
+      contract: 'SyntheticNFTMock',
+      from: deployer,
+      log: true,
+      args: [],
+    });
+  } else {
+    await deploy('SyntheticNFT', {
+      from: deployer,
+      log: true,
+      args: [],
+    });
+  }
+
+  
 };
 
 module.exports.tags = ['jot_pool_implementation'];
