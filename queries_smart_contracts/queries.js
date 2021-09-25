5bb4a2b8bb67a33d7795792d1d4709568896915d
Angel Satorres Almenara <asatorresalmenara@hawk.iit.edu>
2021-09-25 13:36:00 +0200

    clear queries

--- queries_smart_contracts/queries.js
+++ queries_smart_contracts/queries.js
@@ -1,31 +1,100 @@
 
+const BigNumber = require('bignumber.js');
+
+
+function toInput(value, zeros) {
+  return value.toString() + '0'.repeat(zeros)
+}
+
+
+const router_address = "0x38332a7ed05Cb4Ef9a591cA57Db2472a1597De29";
+let decimals_funding = 6;
+let decimals_jots = 18;
+
+let NFT = "0x38332a7ed05Cb4Ef9a591cA57Db2472a1597De29";
+let nftID = "2";
+let ownerSupply = toInput(5000, decimals_jots);
+let initialPriceFraction =  toInput(5, decimals_jots);
+let collectionName =  "SiriusCreature";
+let collectionSymbol =   "OSC";
+
+let syntheticId = "0"
+let amountToBuy =  toInput(10, decimals_jots);
+
+
+
+async function register_NFT() {
+  let router = await ethers.getContractAt("SyntheticProtocolRouter", router_address)
+  let txn = await router.registerNFT(NFT, nftID, 
+                  ownerSupply, initialPriceFraction, collectionName, collectionSymbol);
+  console.log(txn.hash)
+}
+
+
+async function get_collection_manager() {
+  let router = await ethers.getContractAt("SyntheticProtocolRouter", router_address)
+  let collection_address = await router.getCollectionManagerAddress(NFT);
+  console.log(collection_address)
+  return await ethers.getContractAt("SyntheticCollectionManager",collection_address)
+}
+
+
+async function get_collection_info() {
+  let collection = await get_collection_manager();
+  let ownerSupply = await collection.getOwnerSupply(syntheticId);
+  let sellingSupply = await collection.getSellingSupply(syntheticId);
+  let jotFractionPrice = await collection.getJotFractionPrice(syntheticId);
+  let jotAmountLeft = await collection.getJotAmountLeft(syntheticId);
+  let jotBalanceManager = await collection.getContractJotsBalance();
+  console.log("------------- MANAGER INFO ------------------")
+  console.log("OWNER SUPPLY: ", ethers.utils.formatUnits(ownerSupply, decimals_jots))
+  console.log("SELLING SUPPLY: ", ethers.utils.formatUnits(sellingSupply, decimals_jots))
+  console.log("JOT FRACTION PRICE: ", ethers.utils.formatUnits(jotFractionPrice, decimals_funding))
+  console.log("JOT AMOUNT LEFT: ", ethers.utils.formatUnits(jotAmountLeft, decimals_jots))
+  console.log("JOT BALANCE MANAGER: ", ethers.utils.formatUnits(jotBalanceManager, decimals_jots))
+  console.log("---------------------------------------------")
+  
+}
+
+async function buyJotTokens() {
+  let collection = await get_collection_manager();
+  let result = await collection.buyJotTokens(syntheticId, amountToBuy);
+  console.log(result.hash)
+}
+
+
+
+
+
+
+
+
 async function test() {
 
-  let NFT = '0x4A8Cc549c71f12817F9aA25F7f6a37EB1A4Fa087';
-  let nftID = 4;
+  // Register NFT
+  // console.log("Registering NFT....")
+  // await register_NFT();
 
-  //console.log(router.address)
-  //console.log(router)
-  let collection = await ethers.getContractAt("SyntheticCollectionManager","0x6634A42F2C3Dc7f81C303Eb1cA017d6cb017cBb4")
-  let router = await ethers.getContractAt("SyntheticProtocolRouter","0x7aD3939022Fbd98a7e840ab200AB28C1fc7D4Ea6")
-  // let parameters = await ethers.getContractAt("ProtocolParameters","0x4b12F013EecD7646da87d2f9aeBb489c9464F874")
+  // Get collection Manager Address
+  // let collection = await get_collection_manager();
+  // console.log("NFT Collection Manager is: ", collection.address )
+ 
+  // Get Collection Manager Info
+  await get_collection_info();
 
+  // Buy Jot Tokens
+  await buyJotTokens();
   
-  // let txn = await router.registerNFT("0xd03659fed272a197129b8a65f4e732fabfcee99a", "100", 
-  //                  "500000000000000000000", "5000000", "SiriusCreature", "OSC");
-  //const jotAddress = await router.getCollectionManagerAddress(NFT);
-  // console.log( txn)
-
-  result = await collection.buyJotTokens(0, "1000000000000000000")
-  result1 = await collection.getSoldSupply(0)
+  // result = await collection.buyJotTokens(0, "1000000000000000000")
+  // result1 = await collection.getSoldSupply(0)
   
-  result2 = await collection.getSalePrice(0,"1000000000000000000")
+  // result2 = await collection.getSalePrice(0,"1000000000000000000")
   //result =await collection.getRemainingSupply("1")
   //collection = collection.attach("0x0a831A0ffDbbA048434dC1244E61166e2D5A76e4")
   //console.log(await parameters.flippingInterval())
   //result =await collection.getRemainingSupply("1")
   //result = await router.registerNFT("0x8dd1792400d997bf2216d0ea09f6dcb45c8e96e7", 13, 100, 5, "Sirius", "SIRIUS")
-  console.log( result, result1, result2)
+  //console.log( result, result1, result2)
 }
 
 test()
